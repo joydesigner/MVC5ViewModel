19d1b78a79c89e647fca93e022cf03ac2b331052
Xin Zheng <Xin Zheng>
2016-01-01 23:09:16 +1100

    inital submit

---
+++ MVCFormExample/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using MVCFormExample.ViewModels;
+
+namespace MVCFormExample.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create() {
+            return View(new BookViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Create(BookViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Save book
+                return RedirectToAction("Index");
+            }
+            return View(viewModel);
+        }
+    }
+}
---
+++ MVCFormExample/ViewModels/BookViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCFormExample.ViewModels
+{
+    public class BookViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime DatePublished { get; set; }
+        public int Rating { get; set; }
+
+    }
+}
