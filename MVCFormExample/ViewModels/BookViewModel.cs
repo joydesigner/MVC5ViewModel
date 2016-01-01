722fb7e68d1287ac5fc18318155278b44631789c
Xin Zheng <Xin Zheng>
2016-01-01 23:39:33 +1100

    create page view update

--- MVCFormExample/ViewModels/BookViewModel.cs
+++ MVCFormExample/ViewModels/BookViewModel.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCFormExample.ViewModels
 {
     public class BookViewModel
     {
+
+        [Required]
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Author { get; set; }
-        public DateTime DatePublished { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        [Display(Name = "Date Published")]
+        public DateTime? DatePublished { get; set; }
+        [Range(1,5)]
+        [Display(Name = "Rating (1-5)")]
         public int Rating { get; set; }
 
     }
